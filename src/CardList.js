a2e1dd7114a53cb0b2cbcd119b862f05b122bd40
Ruth Newman <ruthnewman@gmail.com>
2021-03-15 23:59:07 +0000

    refactored CardList component

--- src/CardList.js
+++ src/CardList.js
@@ -2,20 +2,23 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ fighters }) => {
-    const cardComponent = fighters.map((user, i) => {
-        return <Card 
-        key={i}
-        id={fighters[i].id} 
-        name={fighters[i].name}
-        imageURL={fighters[i].imageURL}
-        dojo={fighters[i].dojo}
-        sensei={fighters[i].sensei}
-        />
-    });
     return (
         <div>
             <h1 className="tc">Welcome to the Dojo</h1>
-            {cardComponent}
+            {
+              fighters.map((user, i) => {
+                return (
+                        <Card 
+                        key={i}
+                        id={fighters[i].id} 
+                        name={fighters[i].name}
+                        imageURL={fighters[i].imageURL}
+                        dojo={fighters[i].dojo}
+                        sensei={fighters[i].sensei}
+                        />
+                    );
+                })
+            }
         </div>
     );
 }
