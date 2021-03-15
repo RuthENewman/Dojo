bc052327c190c0a1d8f2660cf4d021578eddad9e
Ruth Newman <ruthnewman@gmail.com>
2021-03-15 23:54:10 +0000

    Adding unique keys

--- src/CardList.js
+++ src/CardList.js
@@ -3,7 +3,9 @@ import Card from './Card';
 
 const CardList = ({ fighters }) => {
     const cardComponent = fighters.map((user, i) => {
-        return <Card id={fighters[i].id} 
+        return <Card 
+        key={i}
+        id={fighters[i].id} 
         name={fighters[i].name}
         imageURL={fighters[i].imageURL}
         dojo={fighters[i].dojo}
