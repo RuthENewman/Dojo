e5cdd5a99c97adfcf16c255fd0f82ebd0f39d8e2
Ruth Newman <ruthnewman@gmail.com>
2021-03-28 23:59:52 +0100

    fixed vulnerabilities

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,9 @@ import React from 'react';
 import './Card.css';
 
 const Card = (props) => {
-    const {name, dojo, sensei, imageURL, jpegURL } = props;
+    const {name, dojo, sensei, imageURL } = props;
     return (
         <div className="tc bg-light-green dib ma3 grow bw2 shadow-5">
-            {/* <img 
-            src={`${jpegURL}`}
-            alt="static profile"
-            className="static"
-            /> */}
             <img 
              src={`${imageURL}`}
              alt="profile"
